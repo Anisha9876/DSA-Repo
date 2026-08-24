09ed97344dc0491a348b46a5cc7a2d3a3de09363
Anisha9876 <routrayanisha30@gmail.com>
2026-08-24 19:04:47 +0530

    Sorting 0's 1's 2's in an array brut force method

---
+++ src/Array/Medium/BrutForce/Sorting_0s1s2s_Brut.java
@@ -0,0 +1,26 @@
+package Array.Medium.BrutForce;
+
+public class Sorting_0s1s2s_Brut {
+    public static void main(String[] args) {
+
+        int[] nums={0,0,1,2,1,2,0,1,0,0};
+        //sorting
+        int left=0,right=0;
+//
+
+        for(int i=0;i<nums.length-1;i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[i] > nums[j]) {
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+
+                }
+            }
+        }
+        for(int i:nums){
+            System.out.print(i+" ");
+        }
+
+    }
+}
