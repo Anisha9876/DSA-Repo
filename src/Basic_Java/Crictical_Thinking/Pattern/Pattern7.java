ac1a3bd44b4349b8a9fbbab62498038d861c2f70
Anisha9876 <routrayanisha30@gmail.com>
2026-08-31 18:06:02 +0530

    Pattern 7 pyramid

---
+++ src/Basic_Java/Crictical_Thinking/Pattern/Pattern7.java
@@ -0,0 +1,31 @@
+package Basic_Java.Crictical_Thinking.Pattern;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+        /*
+        Given an integer n. You need to recreate
+        the pattern given below for any value of N. Let's say for N = 5,
+         the pattern should look like as below:
+
+
+
+    *
+   ***
+  *****
+ *******
+*********
+         */
+        int n=5;
+
+        for(int i=0;i<n;i++){
+            for(int j=n-i-1;j>0;j--){
+                System.out.print(" ");
+            }
+            for(int k=0;k<2*i+1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
