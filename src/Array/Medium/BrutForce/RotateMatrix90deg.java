5eafa61fcc63042101c90cecf72d3b8988da2a3b
Anisha9876 <routrayanisha30@gmail.com>
2026-09-02 16:13:11 +0530

    Rotate Matrix By 90 deg brut force method

--- src/Array/Medium/BrutForce/RotateMatrix90deg.java
+++ src/Array/Medium/BrutForce/RotateMatrix90deg.java
@@ -14,27 +14,6 @@ Input: matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
          */
         int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};
 
-//        for(int i=0;i<matrix.length;i++){
-//            for(int j=i;j<matrix[0].length;j++){
-//                int temp=matrix[i][j];
-//                matrix[i][j]=matrix[j][i];
-//                matrix[j][i]=temp;
-//            }
-//        }
-//        for (int i = 0; i < matrix.length; i++) {
-//            int left = 0;
-//            int right = matrix[0].length - 1;
-//
-//            while (left < right) {
-//                int temp = matrix[i][left];
-//                matrix[i][left] = matrix[i][right];
-//                matrix[i][right] = temp;
-//
-//                left++;
-//                right--;
-//            }
-//        }
-//        int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};
         int[][] ans=new int[matrix.length][matrix[0].length];
         int k=matrix[0].length-1;
         for(int i=0;i<matrix.length;i++){
