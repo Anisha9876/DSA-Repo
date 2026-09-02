9c23350fe32768ff4dcab43758b514a5f34c6d27
Anisha9876 <routrayanisha30@gmail.com>
2026-09-02 16:10:36 +0530

    Rotate Matrix By 90 deg better approach

--- src/Array/Medium/BrutForce/RotateMatrix90deg.java
+++ src/Array/Medium/BrutForce/RotateMatrix90deg.java
@@ -13,30 +13,41 @@ Example 1
 Input: matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
          */
         int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};
+
+//        for(int i=0;i<matrix.length;i++){
+//            for(int j=i;j<matrix[0].length;j++){
+//                int temp=matrix[i][j];
+//                matrix[i][j]=matrix[j][i];
+//                matrix[j][i]=temp;
+//            }
+//        }
+//        for (int i = 0; i < matrix.length; i++) {
+//            int left = 0;
+//            int right = matrix[0].length - 1;
+//
+//            while (left < right) {
+//                int temp = matrix[i][left];
+//                matrix[i][left] = matrix[i][right];
+//                matrix[i][right] = temp;
+//
+//                left++;
+//                right--;
+//            }
+//        }
+//        int[][] matrix={{1,2,3},{4,5,6},{7,8,9}};
+        int[][] ans=new int[matrix.length][matrix[0].length];
         int k=matrix[0].length-1;
         for(int i=0;i<matrix.length;i++){
-            for(int j=i;j<matrix[0].length;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+            for(int j=0;j<matrix[0].length;j++){
+
+                ans[j][k-i]=matrix[i][j];
+
             }
         }
-        for (int i = 0; i < matrix.length; i++) {
-            int left = 0;
-            int right = matrix[0].length - 1;
 
-            while (left < right) {
-                int temp = matrix[i][left];
-                matrix[i][left] = matrix[i][right];
-                matrix[i][right] = temp;
-
-                left++;
-                right--;
-            }
-        }
         for(int i=0;i<matrix.length;i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(matrix[i][j]+" ");
+                System.out.print(ans[i][j]+" ");
             }
             System.out.println();
         }
