7b83dbdcccdd7cceafabd378e7efd21a3b21317f
Anisha9876 <routrayanisha30@gmail.com>
2026-09-02 16:23:54 +0530

    Pattern 8 Inverted Pyramid

--- src/Basic_Java/Crictical_Thinking/Pattern/Pattern8.java
+++ src/Basic_Java/Crictical_Thinking/Pattern/Pattern8.java
@@ -15,8 +15,10 @@ public class Pattern8 {
          */
         int n=5;
         for(int i=n-1;i>=0;i--){
-            for(int j=0;j<n-i;j++){
-                System.out.print(" ");
+            if(i!=n-1){
+                for(int j=0;j<n-i-1;j++){
+                    System.out.print(" ");
+                }
             }
 
             for(int k=2*i+1;k>0;k--){
